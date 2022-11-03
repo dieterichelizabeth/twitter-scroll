3a230149fc6910688346cdb487dcb3105db42c0c
Elizabeth Dieterich <dieterichelizabeth@gmail.com>
2022-11-03 13:03:28 -0500

    Adds Styles to Main Component's Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import Navbar from "./components/Nav";
+// import Navbar from "./components/Nav";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <Navbar />
-      </header>
+      </header> */}
+      <Main />
     </div>
   );
 }
---
+++ client/src/components/Main/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Feed from "./Feed";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      {/* <Feed /> */}
+    </>
+  );
+}
+
+export default Main;
